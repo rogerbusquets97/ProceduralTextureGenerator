5b438fb3d7e2f8c46dba438e7a253fe8529461ef
rogerbusquets97 <duranbusquetsroger@gmail.com>
2019-03-16 18:28:05 +0100

    Node connections implemented

---
+++ Editor/Nodes/ConnectionPoint.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace PTG
+{
+    public enum ConnectionType { In, Out }
+    public class ConnectionPoint
+    {
+        public Rect rect;
+        public ConnectionType type;
+        public GUIStyle style;
+        public NodeBase node;
+
+        public List<NodeConnection> connections = null;
+
+        public Action<ConnectionPoint> OnClickConnectionPoint;
+
+        public bool enabled = true;
+
+        public ConnectionPoint(NodeBase node, ConnectionType type, GUIStyle style, Action<ConnectionPoint>OnClickConnectionPoint, bool enabled = false)
+        {
+            this.node = node;
+            this.type = type;
+            this.style = style;
+            this.OnClickConnectionPoint = OnClickConnectionPoint;
+            rect = new Rect(0, 0, 10f, 20f);
+            connections = new List<NodeConnection>();
+        }
+
+        public void Draw(int id)
+        {
+            if(enabled)
+            {
+                rect.y = node.rect.y + (25 * id);
+
+                switch (type)
+                {
+                    case ConnectionType.In:
+                        rect.x = node.rect.x - rect.width + 1f;
+                        break;
+                    case ConnectionType.Out:
+                        rect.x = node.rect.x + node.rect.width - 1;
+                        break;
+                }
+
+                if (GUI.Button(rect, "", style))
+                {
+                    if (OnClickConnectionPoint != null)
+                    {
+                        OnClickConnectionPoint(this);
+                    }
+                }
+            }
+        }
+
+        
+
+    }
+}
---
+++ Editor/Nodes/NodeConnection.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEditor;
+
+namespace PTG
+{
+    public class NodeConnection
+    {
+        public ConnectionPoint inPoint;
+        public ConnectionPoint outPoint;
+        public Action<NodeConnection> OnClickRemoveConnection;
+
+        public NodeConnection(ConnectionPoint inPoint, ConnectionPoint outPoint, Action<NodeConnection> OnClickRemoveConnection)
+        {
+            this.inPoint = inPoint;
+            this.outPoint = outPoint;
+            this.OnClickRemoveConnection = OnClickRemoveConnection;
+        }
+        public void Draw()
+        {
+            Handles.DrawBezier(
+            inPoint.rect.center,
+            outPoint.rect.center,
+            inPoint.rect.center + Vector2.left * 50f,
+            outPoint.rect.center - Vector2.left * 50f,
+            Color.white,
+            null,
+            2f
+            );
+
+            if (Handles.Button((inPoint.rect.center + outPoint.rect.center) * 0.5f, Quaternion.identity, 4, 8, Handles.RectangleHandleCap))
+            {
+                if (OnClickRemoveConnection != null)
+                {
+                    OnClickRemoveConnection(this);
+                }
+            }
+        }
+    }
+}
